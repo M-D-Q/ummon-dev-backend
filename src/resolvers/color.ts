6ce68db96bfb0077fdc490f9679e58579b43eaed
M-D-Q <106432398+M-D-Q@users.noreply.github.com>
2024-06-19 15:41:03 +0200

    spaced things out

--- src/resolvers/color.ts
+++ src/resolvers/color.ts
@@ -5,14 +5,20 @@ import { Element } from "../entities/Element";
 
 export const ColorResolver = {
   inventoryParts: async (color: Color) => {
-    return await AppDataSource.manager.find(InventoryPart, {
-      where: { color: { id: color.id } },
+    return await AppDataSource.manager.find(
+      InventoryPart, {
+      where: {
+        color: { id: color.id } },
       relations: ["part", "color"],
-    });
+     }
+    );
   },
   elements: async (color: Color) => {
-    return await AppDataSource.manager.find(Element, {
-      where: { color: { id: color.id } },
+    return await AppDataSource.manager.find(
+      Element, {
+      where: {
+        color: { id: color.id } 
+      },
       relations: ["color", "part"],
     });
   },
--- src/resolvers/part.ts
+++ src/resolvers/part.ts
@@ -6,27 +6,43 @@ import { PartRelationship } from "../entities/PartRelationship";
 
 export const PartResolver = {
   inventoryParts: async (part: Part) => {
-    return await AppDataSource.manager.find(InventoryPart, {
-      where: { part: { partNum: part.partNum } },
+    return await AppDataSource.manager.find(
+      InventoryPart, {
+      where: {
+        part: { partNum: part.partNum } 
+      },
       relations: ["part", "color"],
-    });
+      }
+    );
   },
   elements: async (part: Part) => {
-    return await AppDataSource.manager.find(Element, {
-      where: { part: { partNum: part.partNum } },
+    return await AppDataSource.manager.find(
+      Element, {
+      where: { 
+        part: { partNum: part.partNum } 
+      },
       relations: ["part", "color"],
-    });
+      }
+    );
   },
   childRelationships: async (part: Part) => {
-    return await AppDataSource.manager.find(PartRelationship, {
-      where: { parentPart: { partNum: part.partNum } },
+    return await AppDataSource.manager.find(
+      PartRelationship, {
+      where: { 
+        parentPart: { partNum: part.partNum } 
+      },
       relations: ["childPart", "parentPart"],
-    });
+      }
+    );
   },
   parentRelationships: async (part: Part) => {
-    return await AppDataSource.manager.find(PartRelationship, {
-      where: { childPart: { partNum: part.partNum } },
+    return await AppDataSource.manager.find(
+      PartRelationship, {
+      where: {
+        childPart: { partNum: part.partNum } 
+      },
       relations: ["childPart", "parentPart"],
-    });
+      }
+    );
   },
 };
